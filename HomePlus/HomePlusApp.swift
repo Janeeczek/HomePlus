ac7c36144a24133aa02f5f842ddee711e1eff8d7
Janeeczek <mazurekjan1@gmail.com>
2021-06-17 22:44:43 +0200

    Initial Commit

---
+++ HomePlus/HomePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePlusApp.swift
+//  HomePlus
+//
+//  Created by Jan Mazurek on 17/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct HomePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
